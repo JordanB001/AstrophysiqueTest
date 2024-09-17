e36811de60aea8502e850e9e4723403993c3d1c7
JordanB001 <143757243+JordanB001@users.noreply.github.com>
2024-09-17 11:44:12 +0200

    Add files via upload

---
+++ atomes/AtomeActinium.java
@@ -0,0 +1,13 @@
+package atomes;
+
+public class AtomeActinium {
+	
+	public static int NumAtomiqueAc   = 89;
+	public static String NomAc        = "Actinium";
+	public static String SCAc         = "Ac";
+	public static double MAtomiqueAc  = 227.0278;
+	public static double MVolumiqueAc = 10.07;
+	public static double PDFAc        = 1047;
+	public static double EbullitionAc = 3197;
+	
+}
